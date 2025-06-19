546c10995f6c553f5dd32568a39fe2249a02f7dc
Jonathan V Tran <me@jonathanvutran.com>
2025-06-18 23:39:25 -0500

    added needed files

---
+++ service-worker.js
@@ -0,0 +1,77 @@
+// A unique name for the cache
+const CACHE_NAME = 'dancers-pointe-recital-v1';
+
+// The list of files to cache on install
+const urlsToCache = [
+  '/',
+  'index.html',
+  'dance_shows.json',
+  'manifest.json',
+  '/image/icon-192.png',
+  '/image/icon-512.png'
+];
+
+// Install event: opens the cache and adds the core files to it.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache and adding core files');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activate event: cleans up old caches.
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Fetch event: serves responses from cache first, falling back to the network.
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return the response from the cache
+        if (response) {
+          return response;
+        }
+
+        // Not in cache - fetch from network, then cache the new response
+        return fetch(event.request).then(
+          networkResponse => {
+            // Check if we received a valid response
+            if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+              return networkResponse;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            const responseToCache = networkResponse.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return networkResponse;
+          }
+        );
+      })
+  );
+});
